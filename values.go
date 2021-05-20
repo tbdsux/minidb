56b6340056ff0178c22aed7e1571767f38be924f
TheBoringDude <iamcoderx@gmail.com>
2021-05-20 17:54:49 +0800

    more KeyValue queries

--- values.go
+++ values.go
@@ -21,7 +21,7 @@ func (db *MiniDB) recoverAssertion() {
 }
 
 // getValue tries to get the key from the map if exists. If value is nil,
-//  It will log error that the key is unknown.
+// It will log error that the key is unknown.
 func (db *MiniDB) getValue(key string) interface{} {
 	value, ok := db.store.Values[key]
 
@@ -44,4 +44,65 @@ func (db *MiniDB) GetString(key string) string {
 	defer db.recoverAssertion()
 
 	return db.getValue(key).(string)
+}
+
+// GetInt finds the key with the int value and returns if exists.
+func (db *MiniDB) GetInt(key string) int {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).(int)
+}
+
+// GetFloat32 finds the key with the float32 value and returns if exists.
+func (db *MiniDB) GetFloat32(key string) float32 {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).(float32)
+}
+
+// GetFloat64 finds the key with the float64 value and returns if exists.
+func (db *MiniDB) GetFloat64(key string) float64 {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).(float64)
+}
+
+// GetBoolSlice finds the key with the []bool value and returns if exits.
+func (db *MiniDB) GetBoolSlice(key string) []bool {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).([]bool)
+}
+
+// GetStringSlice finds the key with the []string value and returns if exists.
+func (db *MiniDB) GetStringSlice(key string) []string {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).([]string)
+}
+
+// GetIntSlice finds the key with the []int value and returns if exists.
+func (db *MiniDB) GetIntSlice(key string) []int {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).([]int)
+}
+
+// GetFloat32Slice finds the key with the []float32 value and returns if exists.
+func (db *MiniDB) GetFloat32Slice(key string) []float32 {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).([]float32)
+}
+
+// GetFloat64Slice finds the key with the []float64 value and returns if exists.
+func (db *MiniDB) GetFloat64Slice(key string) []float64 {
+	defer db.recoverAssertion()
+
+	return db.getValue(key).([]float64)
+}
+
+// Get finds the key and returns an interface value.
+func (db *MiniDB) Get(key string) interface{} {
+	return db.getValue(key)
 }
