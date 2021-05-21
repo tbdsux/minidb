6194e5128962b42671bddf8f38e14771d085c552
TheBoringDude <iamcoderx@gmail.com>
2021-05-21 20:59:12 +0800

    fix test for values_test.go

--- values_test.go
+++ values_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestSet_Store(t *testing.T) {
-	defer cleanFileAfter("store.json", t)
+	defer cleanFileAfter("setstore.json", t)
 
-	db := NewMiniStore("store.json")
+	db := NewMiniStore("setstore.json")
 	db.Set("hello", "world")
 
-	if check, err := ioutil.ReadFile("store.json"); err != nil {
+	if check, err := ioutil.ReadFile("setstore.json"); err == nil {
 		if string(check) != `{"hello":"world"}` {
 			t.Fatal("write is not similar to the output file")
 		}
